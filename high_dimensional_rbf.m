b2cff6d6eae7db6c48d9ac49cdaff6c2ce7690f2
beemarsh <anup8eguy@gmail.com>
2024-11-21 23:24:35 -0600

    High Dimensional RBF Interpolation Surface Plotting and Error Plotting Done

--- high_dimensional_rbf.m
+++ high_dimensional_rbf.m
@@ -4,11 +4,6 @@ f = @(x) 0.75*exp(-1/4*(9*x(1)-2)^2 - 1/4*(9*x(2)-2)^2) + ...
     0.5*exp(-(9*x(1)-7)^2/4-(9*x(2)-3)^2/4) - ...
     0.2*exp(-(9*x(1)-4)^2 - (9*x(2)-7)^2);
 
-
-% RBF Kernel
-shape_param = 0.85;
-rbf_gaussian = @(x1,x2) exp(-pdist2(x1,x2).^2 / 2/shape_param^2);
-
 % Points from 0 to 1 with a 0.01 steps
 t_evaluate = 0:0.01:1;
 
@@ -42,3 +37,47 @@ end
 
 % Scatter Plotting the sampled points in the 3D surface
 plot3(random_points(1,:), random_points(2,:), Z_samples, 'o', 'LineWidth',6);
+
+
+% Kernel Functions and Shape Parameter
+mu = 2;
+%K = @(x,y) exp(1/mu*x'*y);
+K = @(x,y) exp(-1/mu*norm(x-y)^2);
+
+% Gram Matrix
+GramMatrix = zeros(num_samples);
+
+for i = 1:num_samples
+    for j = 1:num_samples
+        GramMatrix(i,j) = K(random_points(:,i),random_points(:,j));
+    end
+end
+
+% Weights
+
+Weights = pinv(GramMatrix)*Z_samples;
+
+% Evaluating the Approximation
+Z_approximation = zeros(size(X));
+
+for i = 1:length(t_evaluate)
+    for j = 1:length(t_evaluate)
+        sum = 0;
+        for ii = 1:num_samples
+            sum = sum + Weights(ii)*K([X(i,j);Y(i,j)],random_points(:,ii));
+        end
+        Z_approximation(i,j) = sum;
+    end
+end
+
+% Plot
+
+figure
+
+surf(X,Y,Z_approximation);
+title('RBF Approximation of Frankes Function')
+
+figure
+
+surf(X,Y,abs(Z-Z_approximation));
+title('Error between true and approximated values')
